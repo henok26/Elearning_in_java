ea52479c17cc4b4a0813b997b1ca313438ef9ed3
henok26 <henokaniguse1931@gmail.com>
2022-12-03 05:20:49 +0300

    computer stream object and class template

---
+++ Streams.java
@@ -0,0 +1,16 @@
+public class Streams {
+    String name;
+        int number_of_students;
+        int number_of_teachers;
+
+    public static void main(String[] args) {
+        
+        
+        Streams stream1=new Streams();
+        stream1.name="computerEngineering";
+        System.out.println(stream1.name);
+        System.out.println(stream1.name);
+        
+    }
+    
+}
